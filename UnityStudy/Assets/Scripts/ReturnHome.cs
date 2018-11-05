ca86e8aa552b8228e582771e1eaded06d8c9b96b
DESKTOP-TEAPK4H\bbang7 <zxas1245@naver.com>
2018-11-06 02:56:41 +0900

    unity syudying

---
+++ UnityStudy/Assets/Scripts/ReturnHome.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReturnHome : MonoBehaviour {
+
+    void Awake()
+    {
+
+    }
+    public void ReturnButton()
+    {
+        Invoke("returnpage", .2f);
+    }
+    void returnpage()
+    {
+        Application.LoadLevel("MainScene");
+    }
+
+}
---
+++ UnityStudy/Assets/Scripts/SplashScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplashScript : MonoBehaviour {
+    public float delayTime = 3;
+	// Use this for initialization
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(delayTime);
+
+        Application.LoadLevel("LobbyScene");
+    }
+	
+}
