40cc919a0c8a83f8ab71a782d9649639b9a39b89
DESKTOP-GBR3NJB\User4579 <dlwjdwls6504@gmail.com>
2018-11-11 05:19:23 +0900

    일어나서 추가한 후에 정리해서 다시 커밋하겠읍니다..

---
+++ Game UI/Prototype/Assets/Script/ShowScore.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowScore : MonoBehaviour {
+
+    public GameObject ScoreBoard;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ScoreBoardSetActive()
+    {
+        ScoreBoard.SetActive(!ScoreBoard.active);
+    }
+}
