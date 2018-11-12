5bd47d4b1cf8ea044ffbf69825f4ff1bf374a3ec
paperforg <paperfrog@naver.com>
2018-11-13 01:53:32 +0900

    ICE 90%완성
    pushDown 버튼은 버튼이 아니라 eventTrigger

---
+++ ShootCat/Assets/Scripts/Item&Button/ItemScript.cs
@@ -0,0 +1,46 @@
+﻿using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+public class ItemScript : MonoBehaviour
+{
+    public Button pushBtn;
+    public ButtonManager ButtonManager;
+    public EventTrigger pushBtnEventTrigger;
+    public Image catPawIce;
+    public Image catPawNormal;
+    #region Behavior
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+    #endregion
+    [Range(0.0f, 5.0f)]
+    public float iceTime;
+    public void ActiveIce()
+    {
+        ButtonManager.BtnInteractable(pushBtn,"off");
+        ButtonManager.BtnImageChange(pushBtn, catPawIce);
+        //pushBtnEventTrigger.
+        //Debug.Log(Time.time);
+        StartCoroutine(WaitTime(iceTime));
+        Debug.Log(Time.time);
+       
+    }
+
+
+    IEnumerator WaitTime(float second)//이 코루틴을 쓰면 second초 만큼 잠시 동작을 멈춥니다.
+    {
+        yield return new WaitForSeconds(second); ButtonManager.BtnImageChange(pushBtn, catPawNormal);
+        ButtonManager.BtnInteractable(pushBtn, "on");
+    }
+
+}
+
