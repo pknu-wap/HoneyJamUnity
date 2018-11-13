e752db0cfc07fe2e2f905e8333b3e8d07d6dcdca
DESKTOP-TEAPK4H\bbang7 <zxas1245@naver.com>
2018-11-13 19:54:01 +0900

    scroll and prefeb study

---
+++ UnityStudy/Assets/ItemObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemObject : MonoBehaviour {
+
+    public Button Item;
+    public Text Title;
+    public GameObject Confirm;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
