3b60dbd38efffa1510035bca60bcd7c170673cc4
paperforg <paperfrog@naver.com>
2018-11-29 01:38:15 +0900

    PlayerListEntry
    aaa

---
+++ ShootCat/Assets/PlayerListEntry.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+using ExitGames.Client.Photon;
+using Photon.Realtime;
+using Photon.Pun.UtilityScripts;
+using Photon.Pun;
+
+public class PlayerListEntry : MonoBehaviour {
+
+    public TMPro.TextMeshProUGUI playerNameText;
+    public Button PlayerReadyButton;
+    //public Image PlayerReadyImage;
+
+    private int ownerId;
+    private bool isPlayerReady;
+
+    #region UNITY
+
+    public void OnEnable()
+    {
+        PlayerNumbering.OnPlayerNumberingChanged += OnPlayerNumberingChanged;
+    }
+
+    public void Start()
+    {
+        if (PhotonNetwork.LocalPlayer.ActorNumber != ownerId)
+        {
+            PlayerReadyButton.gameObject.SetActive(false);
+        }
+        else
+        {
+
+            PlayerReadyButton.onClick.AddListener(() =>
+            {
+                isPlayerReady = !isPlayerReady;
+                SetPlayerReady(isPlayerReady);
+
+
+                if (PhotonNetwork.IsMasterClient)
+                {
+                    FindObjectOfType<LobbyMainPanel>().LocalPlayerPropertiesUpdated();
+                }
+            });
+        }
+    }
+
+    public void OnDisable()
+    {
+        PlayerNumbering.OnPlayerNumberingChanged -= OnPlayerNumberingChanged;
+    }
+
+    #endregion
+
+    public void Initialize(int playerId, string playerName)
+    {
+        ownerId = playerId;
+        playerNameText.text = playerName;
+    }
+
+    private void OnPlayerNumberingChanged()
+    {
+        foreach (Player p in PhotonNetwork.PlayerList)
+        {
+            if (p.ActorNumber == ownerId)
+            {
+                //PlayerColorImage.color = AsteroidsGame.GetColor(p.GetPlayerNumber());
+            }
+        }
+    }
+
+    public void SetPlayerReady(bool playerReady)
+    {
+        PlayerReadyButton.GetComponentInChildren<Text>().text = playerReady ? "Ready!" : "Ready?";
+        //PlayerReadyImage.enabled = playerReady;
+    }
+}
---
+++ ShootCat/Assets/Scripts/Network/PlayerManager.cs
@@ -0,0 +1,36 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviourPunCallbacks, IPunObservable
+{
+    [SerializeField]
+    private GameObject playerUiPrefab;
+
+    public int playerCount;
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Use this for initialization
+    void Start () {
+        if (playerUiPrefab != null)
+        {
+            GameObject _uiGo = Instantiate(playerUiPrefab);
+            _uiGo.SendMessage("SetTarget", this, SendMessageOptions.RequireReceiver);
+        }
+        else
+        {
+            Debug.LogWarning("<Color=Red><a>Missing</a></Color> PlayerUiPrefab reference on player Prefab.", this);
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
