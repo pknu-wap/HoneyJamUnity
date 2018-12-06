a0fbfa8246b9a189d2324de25a5fdcf4bc1b6765
paperforg <paperfrog@naver.com>
2018-12-07 01:26:46 +0900

    RoomSetting
    https://doc.photonengine.com/ko-kr/pun/v2/demos-and-tutorials/pun-basics-tutorial/game-scenes#first_room_creation

---
+++ ShootCat/Assets/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+using Photon.Pun;
+using Photon.Realtime;
+public class GameManager : MonoBehaviourPunCallbacks
+{
+
+	
+	void Start () {
+		
+	}
+	
+	
+	void Update () {
+		
+	}
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+}
