f6d2a1a94e60a8096aa75c2f9de765de922b5a90
paperforg <paperfrog@naver.com>
2018-12-10 21:32:24 +0900

    Network 80% complete
    fuck test

---
+++ shootCatNetwork/Assets/Photon/PhotonUnityNetworking/Demos/DemoAsteroids/Scripts/Lobby/PlayerListEntry.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+using ExitGames.Client.Photon;
+using Photon.Realtime;
+using Photon.Pun.UtilityScripts;
+
+namespace Photon.Pun.Demo.Asteroids
+{
+    public class PlayerListEntry : MonoBehaviour
+    {
+        public TMPro.TextMeshProUGUI PlayerNameText;
+
+        public Button PlayerReadyButton;
+        public Image PlayerReadyImage;
+
+        private int ownerId;
+        private bool isPlayerReady;
+
+        #region UNITY
+
+        public void OnEnable()
+        {
+            PlayerNumbering.OnPlayerNumberingChanged += OnPlayerNumberingChanged;
+        }
+
+        public void Start()
+        {
+            if (PhotonNetwork.LocalPlayer.ActorNumber != ownerId)
+            {
+                PlayerReadyButton.gameObject.SetActive(false);
+            }
+            else
+            {
+                Hashtable initialProps = new Hashtable() {{AsteroidsGame.PLAYER_READY, isPlayerReady}, {AsteroidsGame.PLAYER_LIVES, AsteroidsGame.PLAYER_MAX_LIVES}};
+                PhotonNetwork.LocalPlayer.SetCustomProperties(initialProps);
+                PhotonNetwork.LocalPlayer.SetScore(0);
+
+                PlayerReadyButton.onClick.AddListener(() =>
+                {
+                    isPlayerReady = !isPlayerReady;
+                    SetPlayerReady(isPlayerReady);
+
+                    Hashtable props = new Hashtable() {{AsteroidsGame.PLAYER_READY, isPlayerReady}};
+                    PhotonNetwork.LocalPlayer.SetCustomProperties(props);
+
+                    if (PhotonNetwork.IsMasterClient)
+                    {
+                        FindObjectOfType<LobbyMainPanel>().LocalPlayerPropertiesUpdated();
+                    }
+                });
+            }
+        }
+
+        public void OnDisable()
+        {
+            PlayerNumbering.OnPlayerNumberingChanged -= OnPlayerNumberingChanged;
+        }
+
+        #endregion
+
+        public void Initialize(int playerId, string playerName)
+        {
+            ownerId = playerId;
+            PlayerNameText.text = playerName;
+        }
+
+        private void OnPlayerNumberingChanged()
+        {
+            foreach (Player p in PhotonNetwork.PlayerList)
+            {
+              
+            }
+        }
+
+        public void SetPlayerReady(bool playerReady)
+        {
+            PlayerReadyButton.GetComponentInChildren<TMPro.TextMeshProUGUI>().text = playerReady ? "Ready!" : "Ready?";
+            PlayerReadyImage.enabled = playerReady;
+        }
+    }
+}
---
+++ shootCatNetwork/Assets/Photon/PhotonUnityNetworking/Demos/DemoAsteroids/Scripts/Lobby/RoomListEntry.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Photon.Pun.Demo.Asteroids
+{
+    public class RoomListEntry : MonoBehaviour
+    {
+        public TMPro.TextMeshProUGUI RoomNameText;
+        public TMPro.TextMeshProUGUI RoomPlayersText;
+        public Button JoinRoomButton;
+
+        private string roomName;
+
+        public void Start()
+        {
+            JoinRoomButton.onClick.AddListener(() =>
+            {
+
+                {
+                    PhotonNetwork.LeaveLobby();
+                }
+
+                PhotonNetwork.JoinRoom(roomName);
+            });
+        }
+
+        public void Initialize(string name, byte currentPlayers, byte maxPlayers)
+        {
+            roomName = name;
+
+            RoomNameText.text = name;
+            RoomPlayersText.text = currentPlayers + " / " + maxPlayers;
+        }
+    }
+}
