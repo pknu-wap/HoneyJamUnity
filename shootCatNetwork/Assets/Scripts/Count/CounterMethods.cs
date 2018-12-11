9ddfec6793b1963c5f54905442600d0f54c812a1
paperforg <paperfrog@naver.com>
2018-12-11 21:17:27 +0900

    PhotonView 내용 수정함
    그래서 안됨 왜 안될까

---
+++ shootCatNetwork/Assets/Scripts/Count/CounterMethods.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CounterMethods : MonoBehaviour
+{
+
+    [SerializeField] Counter counter;
+    public GameObject block;
+
+    void Awake()
+    {
+        CounterInit();
+    }
+
+    void Update()
+    {
+    }
+    [Range(0, 500)]
+    public int remainCountSize;
+
+    public void CounterInit()
+    {
+
+        counter.RemaingCount = remainCountSize;
+        counter.CountSize = 1;
+        counter.YourCount = 0;
+        counter.YourCountSize = 1;
+
+    }
+
+    public void CounterSub()
+    { //카운터 빼기 함수
+        counter.RemaingCount -= counter.CountSize;
+        counter.UpdateRemainingCountText();
+    }
+
+    public void CounterPlus(int plusSize)
+    { //카운터 더하기 함수
+        counter.RemaingCount += plusSize;
+        counter.UpdateRemainingCountText();
+    }
+
+    public void DoubleCount()//카운트 두배 함수
+    {
+        counter.CountSize = counter.CountSize * 2;
+        counter.YourCountSize = counter.YourCountSize * 2;
+
+    }
+    public void DivideCount()//카운트 2분의 1 함수
+    {
+        counter.CountSize = counter.CountSize / 2;
+        counter.YourCountSize = counter.YourCountSize / 2;
+    }
+    public void InitCount()//카운트사이즈 초기화
+    {
+        counter.CountSize = 1;
+
+    }
+    public void YourCountPlus()
+    {
+        counter.YourCount += counter.YourCountSize; //버튼 누르면 이 함수를 호출 시켜서 더해줌
+        counter.UpdateYourCountText();
+    }
+}
---
+++ shootCatNetwork/Assets/Scripts/Item&Button/ButtonMethods.cs
@@ -0,0 +1,66 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonMethods : MonoBehaviour
+{
+    public Button PushButton;//pushButton
+    public CounterMethods counterActor;
+    public SpriteRenderer catPawIce;
+    Image image;
+    Image Image
+    {
+        get
+        {
+            if (image == null)
+                image = GetComponent<Image>();
+            return image;
+        }
+    }
+    public SpriteRenderer catPawNormal;
+    #region Behavior
+    void Start()
+    {
+       
+    }
+
+    void Update()
+    {
+
+    }
+    #endregion
+    public void GotoLobby()
+    {
+        SceneManager.LoadScene("Lobby");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+    public void BtnInteractable(Button btn, string state)
+    {
+        if (state == "on") btn.interactable = true;
+        if (state == "off")
+        {
+            btn.interactable = false;
+            Debug.Log("Off");
+        }
+        if (state == "switch")
+            btn.interactable = btn.interactable ? false : true;
+    }
+
+    public void BtnImageChange(Button btn,Image image)
+    {
+
+        btn.GetComponent<Image>().sprite = image.GetComponent<Image>().sprite;
+    
+    }
+
+
+    #region Item
+    
+    #endregion
+}
